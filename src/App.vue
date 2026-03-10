c0fcef0e72c7f2f7e740ff783414928912e0ab17
abh1hi <abhinavbhatnaria5@gmail.com>
2026-03-10 13:32:21 +0530

    Initial commit: Manual Vue 3 + Vuetify (Material M3) setup

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container>
+        <h1>Welcome to OphilliaHRMS</h1>
+        <v-btn color="primary">Get Started</v-btn>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+// No logic needed for base setup
+</script>
